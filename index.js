ad85d371bc46b614a8c7fecca6aa9b5f4945d99a
Allan Lavell <alavell@gmail.com>
2016-03-29 12:27:44 -0300

    Put buzz sumo token into an .env file.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var util = require('util');
 
 var slackToken = process.env.SLACK_TOKEN;
 var witToken = process.env.WIT_TOKEN;
+var buzzSumoToken = process.env.BUZZ_SUMO_TOKEN;
 
 var controller = Botkit.slackbot({
   debug: false
@@ -24,22 +25,21 @@ var witbot = Witbot(witToken);
 
 controller.hears('find me a link about (.*)', 'direct_message', function(bot, message) {
   console.log(message.match[1]);
-  var apiRequest = util.format('http://api.buzzsumo.com/search/articles.json?q=%s&api_key=x', message.match[1]);
+  var apiRequest = util.format('http://api.buzzsumo.com/search/articles.json?q=%s&api_key=%s', message.match[1], buzzSumoToken);
 
   request.get(apiRequest, function(error, response, body) {
     var jsonBody = JSON.parse(body);
 
-    bot.reply(message, 'not implemented yet');
-    //bot.reply(message, jsonBody["results"][0]["og_url"]);
+    bot.reply(message, util.format("Here you go: ", jsonBody["results"][0]["og_url"] ));
   });
 });
 
 controller.hears('.*', ['direct_message, direct_mention'], function(bot, message) {
   var wit = witbot.process(message.text, bot, message);
 
-  wit.hears('find_content', 0.5, function(bot, message, outcome) {
-    bot.reply(message, "here it iz bro");
-  });
+  // wit.hears('find_content', 0.5, function(bot, message, outcome) {
+  //   bot.reply(message, "here it iz bro");
+  // });
 
   wit.hears('hello', 0.5, function(bot, message, outcome) {
     bot.reply(message, 'Hi Ross! I’m Reggie, your new social media assistant. I can share or schedule links for sharing online in seconds, check your social media stats, and even find content worth sharing! Try it now, say: Reggie, find me a link about marketing.');
